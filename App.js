479ef8c1f3e4c603e9bdabc560d481b2c285d7b5
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-08 21:27:47 +0900

    #4 getting weather - fetching URL data

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
+import Axios from "axios";
 
 const API_KEY = "cf103dc043464dfe8445c7d6ad4a9cc0";
 
@@ -9,13 +10,21 @@ export default class extends React.Component {
   state = {
     isLoading: true,
   };
+  //getWeather 위해서 Axios 설치하였음
+  getWeather = async (latitude, longitude) => {
+    const { data } = await Axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+    //axios 덕분에 저 URL 접속했을때의 데이터들이 잘 끌고 와 지는걸 볼 수 있어! (콘솔로그 데이터 보면 다 찍히지)
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
-      console.log(latitude, longitude);
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
       // send address to api
     } catch (error) {
