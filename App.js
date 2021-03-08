f0db569d45f452b2648faec834bf5c6cf9743a49
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-08 20:16:11 +0900

    #2 Loading page

--- App.js
+++ App.js
@@ -1,36 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowview}>
-        <Text style={styles.text}>What the Fuck?</Text>
-      </View>
-      <View style={styles.greenview}>
-        <Text style={styles.text}>What the Fuck?</Text>
-      </View>
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    color: "white",
-  },
-  yellowview: {
-    flex: 1,
-    backgroundColor: "yellow",
-  },
-  greenview: {
-    flex: 2,
-    backgroundColor: "green",
-  },
-});
---
+++ Loading.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Ssinbal Weather Loading....</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FEFADF",
+  },
+  text: {
+    fontSize: 40,
+  },
+});
