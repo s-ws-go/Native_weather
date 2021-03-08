aab19b729c5bb7d16098f2423ce15a538aedf157
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-08 21:14:20 +0900

    #3 finding Current Location & Permission

--- App.js
+++ App.js
@@ -1,6 +1,32 @@
 import React from "react";
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
 
-export default function App() {
-  return <Loading />;
+const API_KEY = "cf103dc043464dfe8445c7d6ad4a9cc0";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+      this.setState({ isLoading: false });
+      // send address to api
+    } catch (error) {
+      Alert.alert("We can't get your Location!", "help Us!!");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
